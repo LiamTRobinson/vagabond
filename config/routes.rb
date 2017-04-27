5d6ceb9642753a2e5c23b03193805a43ec68cde9
LiamTRobinson <Liamtrobinson@gmail.com>
2017-04-27 10:04:54 -0400

    add posts controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,16 @@
 Rails.application.routes.draw do
+  get 'posts/show'
+
+  get 'posts/edit'
+
+  get 'posts/destroy'
+
+  get 'posts/create'
+
+  get 'posts/new'
+
+  get 'posts/update'
+
   get 'cities/index'
 
   get 'cities/show'
