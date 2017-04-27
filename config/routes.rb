cef3cdf232e912a352e31e68a2ebb62e068b04c5
deviea8 <deviea8@gmail.com>
2017-04-27 12:04:15 -0400

    Post show page created as well as edit/update route

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,3 @@
 Rails.application.routes.draw do
-  get 'posts/show'
-
-  get 'posts/edit'
-
-  get 'posts/destroy'
-
-  get 'posts/create'
-
-  get 'posts/new'
-
-  get 'posts/update'
-
-  get 'cities/index'
-
-  get 'cities/show'
-
-  root to: "cities#index"
+  resources :posts
 end
