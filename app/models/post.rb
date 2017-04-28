b379b2a002f1709fdc5d3cab59faf65c5147187e
LiamTRobinson <Liamtrobinson@gmail.com>
2017-04-28 09:24:48 -0400

    add bootstrap and comments model

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
 	belongs_to :city
 	belongs_to :user
+	has_many :comments
 	validates :title, length: { within: 1..200}
   	validates :content, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170428021959) do
+ActiveRecord::Schema.define(version: 20170428123103) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,14 @@ ActiveRecord::Schema.define(version: 20170428021959) do
   create_table "cities", force: :cascade do |t|
     t.string   "name"
     t.string   "photo_url"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.integer  "user_id"
+    t.integer  "post_id"
+    t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
