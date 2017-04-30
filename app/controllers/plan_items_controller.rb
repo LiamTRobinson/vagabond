81a98241cec9db7cf3d2a25b7303d427f58170e3
LiamTRobinson <Liamtrobinson@gmail.com>
2017-04-30 02:34:53 -0400

    add sort for posts on city show

--- app/controllers/plan_items_controller.rb
+++ app/controllers/plan_items_controller.rb
@@ -2,17 +2,19 @@ class PlanItemsController < ApplicationController
   def create
     page = params[:page]
   	post = Post.find(params[:post_id])
+    order = params[:order]
   	PlanItem.create(
   		user_id: current_user.id,
   		post_id: post.id
   		)
-  	redirect_to "/cities/#{post.city.id}?page=#{page}##{post.id}"
+  	redirect_to "/cities/#{post.city.id}?page=#{page}&order=#{order}##{post.id}"
   end
 
   def destroy
     page = params[:page]
     post = Post.find(params[:post_id])
+    order = params[:order]
     PlanItem.where("user_id = #{current_user.id} and post_id = #{post.id}").first.destroy
-  	redirect_to "/cities/#{post.city.id}?page=#{page}##{post.id}"
+  	redirect_to "/cities/#{post.city.id}?page=#{page}&order=#{order}##{post.id}"
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170429233008) do
+ActiveRecord::Schema.define(version: 20170430062119) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -55,9 +55,10 @@ ActiveRecord::Schema.define(version: 20170429233008) do
     t.integer  "city_id"
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
     t.integer  "user_id"
+    t.integer  "plan_items_count", default: 0
   end
 
   create_table "users", force: :cascade do |t|
