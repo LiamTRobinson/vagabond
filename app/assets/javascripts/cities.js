6760c44cc4594d095ec878e225d509c8f1d56459
LiamTRobinson <Liamtrobinson@gmail.com>
2017-04-30 12:17:37 -0400

    fix sort bug

--- app/assets/javascripts/cities.js
+++
@@ -1,7 +0,0 @@
-$(function() {
-	$("#sort-button").on("click", function() {
-		var sort = $("#sort-value").val();
-		var city = $("#sort-value").data("city");
-		window.location.replace(`/cities/${city}?order=${sort}`)
-	})
-})
